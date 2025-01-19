a885d8024b454d42f7201776381ab2250322a441
Timuuurkaaa <husnytdinov34@gmail.com>
2025-01-20 00:29:27 +0500

    Лаба 16

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,25 @@ namespace Husnutdinov41
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new ShoesPage());
+            Manager.MainFrame = MainFrame;
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BtnBack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnBack.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
---
+++ ShoesPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Husnutdinov41
+{
+    /// <summary>
+    /// Логика взаимодействия для ShoesPage.xaml
+    /// </summary>
+    public partial class ShoesPage : Page
+    {
+        public ShoesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditShoes());
+        }
+    }
+}
